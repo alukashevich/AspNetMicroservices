de6ddff0035bfa45a31d6eacd9dadc2cac936e27
Alexei LUKASHEVICH <alexei.lukashevich@assurware.com>
2023-04-05 20:21:11 +0200

    added basket checkout event model

---
+++ src/BuildingBlocks/EventBus.Message/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace EventBus.Message.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+        public Guid Id { get; private set; }
+
+        public DateTime CreatedDate { get; private set; }
+    }
+}
