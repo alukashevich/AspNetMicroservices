925941b58e1548374706f06fd666d951c8f852d3
Alexei LUKASHEVICH <alexei.lukashevich@assurware.com>
2023-04-19 20:54:50 +0200

    added aggregation

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,9 +1,34 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]))
+                //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+                //.AddPolicyHandler(GetRetryPolicy())
+                //.AddPolicyHandler(GetCircuitBreakerPolicy())
+                ;
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
+    //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+    //.AddPolicyHandler(GetRetryPolicy())
+    //.AddPolicyHandler(GetCircuitBreakerPolicy())
+    ;
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]))
+    //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+    //.AddPolicyHandler(GetRetryPolicy())
+    //.AddPolicyHandler(GetCircuitBreakerPolicy())
+    ;
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,28 +39,4 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
