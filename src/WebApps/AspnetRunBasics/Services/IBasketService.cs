a8b583becf60f9ba50bb2c05008835db61079afe
Alexei LUKASHEVICH <alexei.lukashevich@assurware.com>
2023-04-21 15:39:32 +0200

    added web pages application to shopping

---
+++ src/WebApps/AspnetRunBasics/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IBasketService
+    {
+        Task CheckoutBasket(BasketCheckoutModel model);
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/ICatalogService.cs
@@ -0,0 +1,12 @@
+﻿using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public interface ICatalogService
+    {
+        Task<CatalogModel> CreateCatalog(CatalogModel model);
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<CatalogModel> GetCatalog(string id);
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+    }
+}
